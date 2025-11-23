a3a46d14f9aa81764ac9a9460b2523d6a5f5bd4a
Hleb Ramniou <g.remniov@gmail.com>
2025-11-23 18:30:08 +0200

    Initial commit

---
+++ src/main/java/com/budget/buddy/budget_buddy_api/BudgetBuddyApiApplication.java
@@ -0,0 +1,13 @@
+package com.budget.buddy.budget_buddy_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BudgetBuddyApiApplication {
+
+  static void main(String[] args) {
+    SpringApplication.run(BudgetBuddyApiApplication.class, args);
+  }
+
+}
---
+++ src/test/java/com/budget/buddy/budget_buddy_api/BudgetBuddyApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.budget.buddy.budget_buddy_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BudgetBuddyApiApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
