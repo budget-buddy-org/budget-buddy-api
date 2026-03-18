f27cc8712593257406a15aa63c665de0e79e85be
Hleb Ramniou <g.remniov@gmail.com>
2026-03-18 17:09:51 +0200

    Implement Auth integration test

--- src/test/java/com/budget/buddy/budget_buddy_api/BaseIntegrationTest.java
+++ src/test/java/com/budget/buddy/budget_buddy_api/BaseIntegrationTest.java
@@ -1,12 +1,21 @@
 package com.budget.buddy.budget_buddy_api;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.webmvc.test.autoconfigure.AutoConfigureMockMvc;
 import org.springframework.context.annotation.Import;
 import org.springframework.test.context.ActiveProfiles;
+import org.springframework.transaction.annotation.Transactional;
+import tools.jackson.databind.ObjectMapper;
 
 @ActiveProfiles("test")
 @SpringBootTest
+@AutoConfigureMockMvc
 @Import(TestcontainersConfig.class)
+@Transactional
 public abstract class BaseIntegrationTest {
+
+  @Autowired
+  protected ObjectMapper objectMapper;
 
 }
