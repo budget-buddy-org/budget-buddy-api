cd1528081c4168882d76d1fac0e21dc492bb5cb2
Hleb Ramniou <g.remniov@gmail.com>
2026-06-13 20:45:55 +0300

    chore: fix formatting

--- src/main/resources/db/changelog/migrations/014-user-settings.sql
+++ src/main/resources/db/changelog/migrations/014-user-settings.sql
@@ -5,11 +5,11 @@ CREATE TABLE user_preferences
 (
   id         UUID PRIMARY KEY,
   owner_id   UUID        NOT NULL REFERENCES users (id) ON DELETE CASCADE,
-    language   VARCHAR(7)  NOT NULL,
-    currency   VARCHAR(3)  NOT NULL,
-    timezone   VARCHAR(64) NOT NULL,
-    version    INTEGER     NOT NULL,
-    created_at TIMESTAMPTZ NOT NULL,
+  language   VARCHAR(7)  NOT NULL,
+  currency   VARCHAR(3)  NOT NULL,
+  timezone   VARCHAR(64) NOT NULL,
+  version    INTEGER     NOT NULL,
+  created_at TIMESTAMPTZ NOT NULL,
   updated_at TIMESTAMPTZ NOT NULL,
   UNIQUE (owner_id)
 );
